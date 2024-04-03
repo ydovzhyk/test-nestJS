61aaee910d2393895c9a3f7a6cb1a33f13806a90
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2024-04-03 18:14:58 +0300

    add corrections

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/modules/app.module';
+import { CorsMiddleware } from './middleware/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(new CorsMiddleware().use);
   await app.listen(3000);
 }
 bootstrap();
